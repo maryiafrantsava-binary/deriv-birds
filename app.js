146d2e45eec582d1ba899669828248468fc7814c
maryiafrantsava-binary <maryia.frantsava@binary.com>
2022-04-13 12:12:26 +0300

    MaryiaF/chore: implementation registration frontend + backend

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const config = require('config');
 const mongoose = require('mongoose');
 
 const app = express();
+
+app.use(express.json({extended: true}))
+
 app.use('/api/auth', require('./routes/auth.routes'));
 
 const PORT = config.get('port') || 5000;
 
 async function start(){
     try{
-        await mongoose.connect(config.get('mongoURi'), {
+        await mongoose.connect(config.get('mongoUri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             // useCreateIndex: true//9F9HY69NiT2DNgDb
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,77 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
+import {useHttp} from '../hooks/http.hook';
+import {useMessage} from '../hooks/message.hook';
+import {AuthContext} from '../context/Auth.context';
 
 export const AuthPage = () => {
+    const auth = useContext(AuthContext);
+    const message = useMessage();
+    const {loading, error, request, clearError} = useHttp();
+    const [form, setForm] = useState({
+        email: '', password: ''
+    });
+
+    useEffect(() => {
+        message(error);
+        clearError();
+    }, [error, message, clearError])
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value });
+    }
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form})
+            message(data.message);
+        } catch (e) {}
+    }
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form})
+            auth.login(data.token, data.userId);
+            // message(data.message);
+        } catch (e) {}
+    }
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Auth Page</h1>
+                {/*<input*/}
+                {/*    placeholder="placeholder"*/}
+                {/*    id="name"*/}
+                {/*    type="text"*/}
+                {/*    className="validate"*/}
+                {/*/>*/}
+                {/*<label>name</label>*/}
+                <input
+                    placeholder="placeholder1"
+                    id="email"
+                    type="text"
+                    name="email"
+                    value={form.email}
+                    className="validate"
+                    onChange={changeHandler}
+                />
+                <label htmlFor="email">email</label>
+                <input
+                    placeholder="placeholder2"
+                    id="password"
+                    type="text"
+                    name="password"
+                    className="validate"
+                    value={form.password}
+                    onChange={changeHandler}
+                />
+                <label htmlFor="password">password</label>
+
+                <button
+                    onClick={loginHandler}
+                    disabled={loading}
+                >Sign in</button>
+                <button
+                    onClick={registerHandler}
+                    disabled={loading}
+                >Sign up</button>
+            </div>
         </div>
     )
 }
