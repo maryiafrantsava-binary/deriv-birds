0d32855b6e2ab4d51edb50f7afde8f37a47a6bcb
maryiafrantsava-binary <maryia.frantsava@binary.com>
2022-04-13 13:04:41 +0300

    MaryiaF/chore: implementation registration frontend + backend

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json({extended: true}))
 
 app.use('/api/auth', require('./routes/auth.routes'));
 
-const PORT = config.get('port') || 5000;
+const PORT = config.get('port') || 8000;
 
 async function start(){
     try{
