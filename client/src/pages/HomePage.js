ffddf71363cc09a264046d63555c763a7bf7a5c8
maryiafrantsava-binary <maryia.frantsava@binary.com>
2022-04-27 21:12:10 +0300

    MaryiaF/fix: main page layout, navlinks component, game frames

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,17 @@
 import React from "react";
-import './HomePage.css'
+import { NavLink } from "react-router-dom";
+import './HomePage.scss'
 
 export const HomePage = () => {
     return (
         <div className="main">
             <div>
-                <h1>Welcome to Bird’s Game</h1>
-                <p className="description">Game description</p>
-                <button className="game-button">Play a game</button>
+                <h1 className="main_title">Welcome to Bird’s Game</h1>
+                <p className="description">The Game that inspires and can prepare you for real trading!</p>
+                <p className="description">Allow you to understand the simplest basics</p> 
+                <p className="description">and our beautiful birds will allow you not to be bored.</p>
+                <p className="description">Enjoy with birds!</p>
+                <button className="game-button"><NavLink to="/game"><span className="game-button-text">Play game</span></NavLink></button>
             </div>
 
             <div className="score-container" />
